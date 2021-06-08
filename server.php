3a6dcf69cb903749e7875caaf468a4b9930d5f1c
qhartine <77783749+qhartine@users.noreply.github.com>
2021-06-08 22:59:39 +0800

    Update server.php

--- server.php
+++ server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'root', '1234', 'registration');
+//$db = mysqli_connect('localhost', 'root', '1234', 'registration');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
@@ -83,5 +83,17 @@ if (isset($_POST['login_user'])) {
     }
   }
 }
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
